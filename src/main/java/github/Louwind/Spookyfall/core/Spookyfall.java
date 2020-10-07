427fd396f014d4128becfa359f4fc4e890029eee
Louwind <46799905+Louwind@users.noreply.github.com>
2020-10-06 21:58:19 -0700

    Fabric setup

---
+++ src/main/java/github/Louwind/Spookyfall/core/Spookyfall.java
@@ -0,0 +1,12 @@
+package github.Louwind.Spookyfall.core;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Spookyfall implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+
+}
