c26a9f69aca5512ce5fb610e636cae8d369d5ee4
Louwind <46799905+Louwind@users.noreply.github.com>
2020-10-28 16:34:21 -0700

    Update Features API to 1.2.0

--- src/main/java/github/Louwind/Spookyfall/core/init/Items.java
+++ src/main/java/github/Louwind/Spookyfall/core/init/Items.java
@@ -1,18 +1,15 @@
 package github.Louwind.Spookyfall.core.init;
 
-import com.google.common.base.Suppliers;
 import github.Louwind.Features.impl.item.DebugStickItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 
-import static net.minecraft.world.gen.feature.DefaultFeatureConfig.INSTANCE;
-
 public class Items {
 
-    public static final Item CAMPSITE_DEBUG_STICK = new DebugStickItem(new Identifier("spookyfall:campsite"), Suppliers.ofInstance(INSTANCE), new Item.Settings());
+    public static final Item CAMPSITE_DEBUG_STICK = new DebugStickItem(new Identifier("spookyfall:campsite"), new Item.Settings());
 
-    public static final Item HAY_BALE_CART_DEBUG_STICK = new DebugStickItem(new Identifier("spookyfall:hay_bale_cart"), Suppliers.ofInstance(INSTANCE), new Item.Settings());
+    public static final Item HAY_BALE_CART_DEBUG_STICK = new DebugStickItem(new Identifier("spookyfall:hay_bale_cart"), new Item.Settings());
 
-    public static final Item PUMPKIN_CART_DEBUG_STICK = new DebugStickItem(new Identifier("spookyfall:pumpkin_cart"), Suppliers.ofInstance(INSTANCE), new Item.Settings());
+    public static final Item PUMPKIN_CART_DEBUG_STICK = new DebugStickItem(new Identifier("spookyfall:pumpkin_cart"), new Item.Settings());
 
 }
